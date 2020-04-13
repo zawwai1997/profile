9b287f460bf093f6a6767ae89448099d5df55b8f
zawwai1997 <mamahninsu.ioio@gmail.com>
2020-04-13 12:55:21 +0630

    betav1

--- admin/core/database/Connection.php
+++ admin/core/database/Connection.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$pdo = new PDO('mysql:host=localhost;dbname=covid19', "kozawgyi6", "yJo0e3~0");
+$pdo = new PDO('mysql:host=localhost;dbname=covid19', "kozawgyi8", "yJo0e3~0");
  // $pdo = new PDO('mysql:host=localhost;dbname=covid19', "root", "");
 $pdo->exec("SET NAMES UTF-8;");
 $pdo->exec("SET character_set_results=UTF-8;");
